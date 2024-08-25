8e6a4f826a875c724b943164965cc5d53e05b2b9
Jitesh Kalta <jiteshkalta@gmail.com>
2024-08-26 00:07:29 +0530

    finalize the app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
@@ -62,7 +63,7 @@ const Sidebar = ({ user }: SidebarProps) => {
           )
         })}
 
-        USER
+        <PlaidLink user={user}/>
       </nav>
 
       <Footer user={user} />
